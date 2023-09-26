e800a9abd5ca97af30cc9e1d70cf8e391d2cd1c3
Mariana Costa <mrncstt@gmail.com>
2023-09-26 23:51:09 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,16 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import streamlit as st
 from streamlit.logger import get_logger
@@ -20,29 +7,29 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Make Over Moday",
+        page_icon="📊",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Make Over Moday 📊")
 
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Makeover Monday is a weekly data visualization challenge where participants are provided with a dataset and an original visualization. 
+        
+        The goal is to improve or "makeover" the visualization by reimagining, redesigning, or simply tweaking it. 
+       
+         This initiative, led by the data community, aims to enhance data visualization skills, foster creativity, and build a supportive learning environment. 
+        Participants often share their creations on social media, particularly Twitter/X, using the hashtag #MakeoverMonday, encouraging feedback and collaboration.
+        
+
+        **👈 Select a week from the sidebar** to see some examples
+
+                ### Want to learn more?
+        - Check out [Make Over Monday](https://www.makeovermonday.co.uk/)
+        - Check [#MakeOverMonday](https://twitter.com/hashtag/MakeoverMonday?src=hashtag_click)
     """
     )
 
