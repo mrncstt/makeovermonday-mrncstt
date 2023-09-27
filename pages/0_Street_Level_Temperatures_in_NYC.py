04c8cc5a222089ff7c6d5c0522076e814cf65246
Mariana Costa <mrncstt@gmail.com>
2023-09-27 23:57:38 +0200

    Update 0_Street_Level_Temperatures_in_NYC.py

--- pages/0_Street_Level_Temperatures_in_NYC.py
+++ pages/0_Street_Level_Temperatures_in_NYC.py
@@ -5,18 +5,12 @@ from urllib.error import URLError
 import altair as alt
 from streamlit.hello.utils import show_code
 
+st.set_page_config(page_title="Street Level Temperatures NYC", page_icon="🌡️")
+
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-st.map(df)
-
-
-st.set_page_config(page_title="Street Level Temperatures NYC", page_icon="🌡️")
 st.markdown("# Street Level Temperatures NYC")
 st.sidebar.header("Street Level Temperatures NYC")
-st.write(
-
-)
-
-
+st.map(df)
