cd8ee2f0aa2622edcc4c2c104b798aeba1236cc6
Mariana Costa <mrncstt@gmail.com>
2023-09-28 00:25:28 +0200

    Update 0_Street_Level_Temperatures_in_NYC.py

--- pages/0_Street_Level_Temperatures_in_NYC.py
+++ pages/0_Street_Level_Temperatures_in_NYC.py
@@ -9,6 +9,9 @@ st.set_page_config(page_title="Street Level Temperatures NYC", page_icon="ðŸŒ¡ï¸
 # Read the data from the provided URL
 df = pd.read_json('https://data.cityofnewyork.us/resource/qdq3-9eqn.json')
 
+# Display the column names to verify
+st.write(df.columns.tolist())
+
 # Rename columns to match previous naming convention
 df = df.rename(columns={
     'AirTemp': 'airtemp',
@@ -36,28 +39,30 @@ selected_day = st.sidebar.selectbox("Select a Day:", df['day'].unique())
 # Filter the dataframe based on the selected day
 df_filtered = df[df['day'] == selected_day]
 
-# Plotting the heatmap
-layer = pdk.Layer(
-    "HeatmapLayer",
-    data=df_filtered,
-    opacity=0.9,
-    get_position=["lon", "lat"],
-    get_weight="airtemp_normalized",
-    threshold=0.3,
-    radiusPixels=30,
-)
+# Assuming 'lat' and 'lon' columns are present, proceed with the rest of the code
+if 'lat' in df_filtered.columns and 'lon' in df_filtered.columns:
+    # Plotting the heatmap
+    layer = pdk.Layer(
+        "HeatmapLayer",
+        data=df_filtered,
+        opacity=0.9,
+        get_position=["lon", "lat"],
+        get_weight="airtemp_normalized",
+        threshold=0.3,
+        radiusPixels=30,
+    )
 
-view_state = pdk.ViewState(
-    latitude=df_filtered['lat'].mean(),
-    longitude=df_filtered['lon'].mean(),
-    zoom=10,
-    min_zoom=5,
-    max_zoom=15,
-    pitch=40.5,
-    bearing=-27.36,
-)
+    view_state = pdk.ViewState(
+        latitude=df_filtered['lat'].mean(),
+        longitude=df_filtered['lon'].mean(),
+        zoom=10,
+        min_zoom=5,
+        max_zoom=15,
+        pitch=40.5,
+        bearing=-27.36,
+    )
 
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
+    st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
 
-# Optional: Display the raw data in a table below the map
-st.write(df_filtered)
+    # Optional: Display the raw data in a table below the map
+    st.write(df_filtered)
